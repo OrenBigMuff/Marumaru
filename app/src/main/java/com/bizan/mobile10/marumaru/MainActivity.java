21d7485a5ad18f89e1594c9e690f4d991620de9c
Dan_Imai <oinarhythm0508@gmail.com>
2016-01-28 15:57:29 +0900

    Revert "20160128作業分コミット"
    This reverts commit b5d875f5d1e4a46916eb291b748ea51c73a33980.

--- app/src/main/java/com/bizan/mobile10/marumaru/MainActivity.java
+++ app/src/main/java/com/bizan/mobile10/marumaru/MainActivity.java
@@ -1,11 +1,7 @@
 package com.bizan.mobile10.marumaru;
 
-import android.view.View.OnClickListener;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,16 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button btn = (Button) findViewById(R.id.btnTest);
-        btn.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Intentの生成
-                Intent intent = new Intent(MainActivity.this, Clear.class);
-                // 次のアクティビィ起動
-                startActivity(intent);
-            }
-        });
     }
 }
