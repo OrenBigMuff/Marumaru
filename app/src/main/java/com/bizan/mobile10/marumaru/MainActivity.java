55446ea5a789a3c0fc19a429381a37ac21cea569
yyuki <yyuki@gmail.com>
2016-01-28 13:01:05 +0900

    トップ画面

--- app/src/main/java/com/bizan/mobile10/marumaru/MainActivity.java
+++ app/src/main/java/com/bizan/mobile10/marumaru/MainActivity.java
@@ -4,10 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    //hhideyyuki
+    //ブランチ
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+
     }
 }
