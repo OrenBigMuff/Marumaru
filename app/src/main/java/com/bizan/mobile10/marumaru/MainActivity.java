672b9d5f77c82549f50595897b5e52b0899d2e96
bizansyun <bizansyun@gmail.com>
2016-01-29 14:44:56 +0900

    checkboxがおかしい　あとは任せた

--- app/src/main/java/com/bizan/mobile10/marumaru/MainActivity.java
+++ app/src/main/java/com/bizan/mobile10/marumaru/MainActivity.java
@@ -4,11 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -17,36 +17,56 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        final CheckBox checkBox = (CheckBox) findViewById(R.id.checkbox);
-//        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//        final CheckBox checkBox = (CheckBox) findViewById(R.id.checkBox);
+//        checkBox.setOnClickListener(new View.OnClickListener() {
 //
-//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-//                if(isChecked) {
-//                    Toast.makeText(getApplication(), "チェックが付いた", Toast.LENGTH_LONG).show();
+//            public void onClick(View v) {
+//                if (checkBox.isChecked() == true) {
+//                    // チェックされた状態の時の処理を記述
 //                } else {
-//                    Toast.makeText(getApplication(), "チェックが外れた", Toast.LENGTH_LONG).show();
+//                    // チェックされていない状態の時の処理を記述
 //                }
 //            }
 //        });
 
 
-            LinearLayout cardLinear = (LinearLayout) this.findViewById(R.id.cardLinear);
-            cardLinear.removeAllViews();
 
-            LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-            LinearLayout linearLayout = (LinearLayout) inflater.inflate(R.layout.test_card, null);
-            CardView cardView = (CardView) linearLayout.findViewById(R.id.cardView);
-            TextView textBox = (TextView) linearLayout.findViewById(R.id.textBox);
-            textBox.setText("このアプリを使う上での注意点。なんとかかんとか" +
-                    "なんとかかんとかでなんとかである。以上！");
-            cardView.setTag(0);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            //音を消す
+            @Override
+            public void onClick(View view) {
 
-            cardLinear.addView(linearLayout, 0);
+            }
+        });
 
 
-        }
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            // TOP画面を表示
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+        LinearLayout cardLinear = (LinearLayout) this.findViewById(R.id.cardLinear);
+        cardLinear.removeAllViews();
+
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        LinearLayout linearLayout = (LinearLayout) inflater.inflate(R.layout.test_card, null);
+        CardView cardView = (CardView) linearLayout.findViewById(R.id.cardView);
+        TextView textBox = (TextView) linearLayout.findViewById(R.id.textBox);
+        textBox.setText("このアプリを使う上での注意点。なんとかかんとか" +
+                "なんとかかんとかでなんとかである。以上！");
+        cardView.setTag(0);
+        cardLinear.addView(linearLayout, 0);
+
 
     }
+
+}
 
 
 
