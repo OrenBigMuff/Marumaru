b37b3d753bca5465dca0e5c852eb63ccf1fbda68
yyuki <yyuki@gmail.com>
2016-02-01 13:59:26 +0900

    よくわｋらんがコミット

---
+++ app/src/main/java/com/bizan/mobile10/marumaru/Commentary.java
@@ -0,0 +1,83 @@
+package com.bizan.mobile10.marumaru;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+public class Commentary extends AppCompatActivity {
+    CheckBox checkBox;
+    Sound sound;
+    PreferenceC pref;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        LinearLayout cardLinear = (LinearLayout) this.findViewById(R.id.cardLinear);
+        cardLinear.removeAllViews();
+
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        LinearLayout linearLayout = (LinearLayout) inflater.inflate(R.layout.test_card, null);
+        CardView cardView = (CardView) linearLayout.findViewById(R.id.cardView);
+        TextView textBox = (TextView) linearLayout.findViewById(R.id.textBox);
+        textBox.setText("このアプリを使う上での注意点。なんとかかんとか" +
+                "なんとかかんとかでなんとかである。以上！");
+        cardView.setTag(0);
+        cardLinear.addView(linearLayout, 0);
+
+        checkBox = (CheckBox) findViewById(R.id.checkBox);
+        checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkBox.isChecked() == true) {
+                    // チェックされた状態の時の処理を記述
+                } else {
+                    // チェックされていない状態の時の処理を記述
+                }
+            }
+        });
+        pref = new PreferenceC(this);
+        final PreferenceC pref = new PreferenceC(this);
+//        音楽ファイル入れて名前書く.
+        sound = new Sound(this,R.raw.see );
+
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            //音を消す
+            @Override
+            public void onClick(View view) {
+                if(sound.isSoundON()){
+                    sound.setSoundON(false);
+                    //iconChenge
+                }else{
+                    sound.setSoundON(true);
+                    //iconChenge
+                }
+                pref.writeConfig("soundON",sound.isSoundON());
+                sound.playSE();
+            }
+        });
+
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            // TOP画面を表示
+            //finish()
+            @Override
+            public void onClick(View view) {
+                pref.writeConfig("comm",checkBox.isChecked());            }
+        });
+
+
+    }
+
+}
+
+
+
---
+++ app/src/main/java/com/bizan/mobile10/marumaru/Results.java
@@ -0,0 +1,128 @@
+package com.bizan.mobile10.marumaru;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by kei on 2016/01/28.
+ */
+public class Results extends AppCompatActivity {
+
+    private int[] id = {1,2,3,4,5,6,7,8,9,10};
+    private int[] correction = {1,0,0,1,0,1,1,1,0,0};
+    private String[] question = {"Question1","Question2","Question3","Question4","Question5",
+            "Question6","Question7","Question8","Question9","Question10"};
+    private String[] answer = {"意味1","意味2","意味3","意味4","意味5","意味6","意味7","意味8","意味9","意味10"};
+    private int j;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+
+        //10問中の正解数を表示させる
+        int correct = 0;
+        for (int i = 0; i < 10; i++) {
+            if (correction[i]==1) {
+                correct++;
+            }
+        }
+        TextView txvResult = (TextView) this.findViewById(R.id.txvArResult);
+        txvResult.setText("結果 : " + correct + "問" + " / 10問");
+
+        //CardViewを表示させる
+        LinearLayout cardLinear = (LinearLayout) this.findViewById(R.id.lilArCardLiner);
+        cardLinear.removeAllViews();
+
+        for (int i = 0; i < 10; i++) {
+            j = i;
+            LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+
+            //正解の場合
+            if (correction[i]==1) {
+                LinearLayout linearLayout = (LinearLayout) inflater.inflate(R.layout.card_correct, null);
+
+                CardView cardView = (CardView) linearLayout.findViewById(R.id.cdvCcCard);
+
+                //問題番号
+                TextView txvNumber = (TextView) linearLayout.findViewById(R.id.txvCcNumber);
+                txvNumber.setText("問題 " + (i+1));
+
+                //単語
+                TextView txvQuestion = (TextView) linearLayout.findViewById(R.id.txvCcQuestion);
+                txvQuestion.setText(question[i]);
+
+                //意味
+                TextView txvAnswer = (TextView) linearLayout.findViewById(R.id.txvCcAnswer);
+                txvAnswer.setText(answer[i]);
+
+                cardView.setTag(i);
+                cardLinear.addView(linearLayout, i);
+
+                //マグレボタン
+                Button btnFluke = (Button) cardView.findViewById(R.id.btnCcFluke);
+                btnFluke.setTag(i);
+                btnFluke.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(Results.this,
+                                "Question" + (Integer.parseInt(String.valueOf(v.getTag())) + 1) + " を再度出題します",
+                                Toast.LENGTH_SHORT).show();
+                        correction[Integer.parseInt(String.valueOf(v.getTag()))] = 0;
+                    }
+                });
+            }
+
+            //不正解の場合
+            else if (correction[i]==0) {
+                LinearLayout linearLayout = (LinearLayout) inflater.inflate(R.layout.card_mistake, null);
+
+                CardView cardView = (CardView) linearLayout.findViewById(R.id.cdvCmCard);
+
+                //問題番号
+                TextView txvNumber = (TextView) linearLayout.findViewById(R.id.txvCmNumber);
+                txvNumber.setText("問題 " + (i+1));
+
+                //単語
+                TextView txvQuestion = (TextView) linearLayout.findViewById(R.id.txvCmQuestion);
+                txvQuestion.setText(question[i]);
+
+                //意味
+                TextView txvAnswer = (TextView) linearLayout.findViewById(R.id.txvCmAnswer);
+                txvAnswer.setText(answer[i]);
+
+                cardView.setTag(i);
+                cardLinear.addView(linearLayout, i);
+            }
+        }
+
+        //TOPへ戻るボタン押下の動作
+        Button btnReturn = (Button) this.findViewById(R.id.btnArReturn);
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        String result = "";
+        for (int i = 0; i < 10; i++) {
+            result = result + correction[i] +", ";
+        }
+        Toast.makeText(this,result,Toast.LENGTH_LONG).show();
+    }
+}
